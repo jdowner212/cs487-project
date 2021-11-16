893692cb76aec6c74073aa21f98260ecba043dbb
jsdowner <62725284+jsdowner@users.noreply.github.com>
2021-11-16 14:30:25 -0600

    Update my_config.py

--- my_config.py
+++ my_config.py
@@ -3,16 +3,16 @@
 MY_ID = -1
 CUSTOMER_ABSENT, CUSTOMER_LOGIN, CUSTOMER_EMAIL = (0, 1, 2)
 
-APP_NAME = "Mendiona bytes"
+APP_NAME = "CS 487: Coffee Shop"
 ADMIN_PERM = 1
 
-BACKGROUND = 'AntiqueWhite3'
-FOREGROUND = 'AntiqueWhite1'
+BACKGROUND = 'darkseagreen4'
+FOREGROUND = 'darkseagreen1'
 ERROR_FOREGROUND = 'red'
 
 
 def is_float(value):
-    """check whether it's float but also not throw error when it's string"""
+    """check if float"""
     try:
         return isinstance(float(value), float)
     except ValueError:
@@ -20,7 +20,7 @@ def is_float(value):
 
 
 def is_integer(value):
-    """check whether it can be an int but also not throw error when it's string"""
+    """check if int"""
     try:
         return isinstance(int(value), int)
     except ValueError:
